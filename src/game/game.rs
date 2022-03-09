825818873cd1c5af8b08cda8d19b37e9defb9171
jeandeducla <jean.deducla@gmail.com>
2022-03-09 20:54:05 +0100

    Refactored game

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,7 +1,11 @@
 use super::challenge::Challenge;
+use super::command::Command;
 use super::score::Score;
 use crate::music::intervals::Interval;
 use crate::music::notes::Note;
+
+use rustyline::Editor;
+use std::str::FromStr;
 
 const DEFAULT_CHALLENGE_NUM: usize = 5;
 
@@ -47,5 +51,53 @@ impl Game {
                 ),
             total_answers: self.challenges.len(),
         }
+    }
+
+    pub fn play(&mut self, editor: &mut Editor<()>) -> Result<(), ()> {
+        println!("\nOk ! Let's go");
+
+        for (idx, challenge) in self.challenges.iter_mut().enumerate() {
+            println!("\n[.] Challenge number {}", idx + 1);
+            challenge.play_correct_answer();
+            println!("What is the interval?");
+
+            loop {
+                let readline = editor.readline(" --> ");
+
+                match readline {
+                    Ok(input) => {
+                        if let Ok(cmd) = Command::from_str(&input) {
+                            if let Command::Replay = cmd {
+                                challenge.play_correct_answer();
+                            }
+                        } else if let Ok(interval) = Interval::from_str(&input) {
+                            challenge.answer(interval);
+
+                            println!("Your answer was...");
+                            match challenge.verify_user_answer() {
+                                true => {
+                                    println!("     > Correct!");
+                                }
+                                false => {
+                                    println!("     x Uncorrect...");
+                                    println!("The correct answer was {}", challenge.correct_answer);
+                                }
+                            }
+                            break;
+                        } else {
+                            println!("Invalid command: type 'replay' to replay the current challenge or type a valid interval");
+                        }
+                    }
+                    Err(_) => {
+                        return Err(());
+                    }
+                }
+            }
+        }
+
+        println!("\nGame Over");
+        println!("Your score is: {}\n", self.get_current_score());
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ mod player;
 mod source;
 
 use crate::game::{Command, Game};
-use crate::music::intervals::Interval;
 use crate::music::notes::Note;
 
 fn main() {
@@ -17,88 +16,38 @@ fn main() {
 
     let mut rl = Editor::<()>::new();
 
+    // TODO: rules are going to depend on the challenges that you choose
     game::print_rules();
 
-    let mut challenge_num = 0;
     loop {
+        if let Some(mut game) = session.take() {
+            let _ = game.play(&mut rl);
+        }
+
+        println!("Type 'start' to start a new game and 'quit' to quit Jazz Gym");
         let readline = rl.readline(">> ");
         match readline {
-            Ok(line) => match session.as_mut() {
-                Some(game) => {
-                    if let Ok(replay) = Command::from_str(&line) {
-                        if let Command::Replay = replay {
-                            if let Some(challenge) = game.challenges.get_mut(challenge_num) {
-                                challenge.play_correct_answer();
-                            }
-                        }
-                    } else {
-                        match Interval::from_str(&line) {
-                            Ok(interval) => {
-                                if let Some(challenge) = game.challenges.get_mut(challenge_num) {
-                                    challenge.answer(interval);
-                                    println!("Your answer was...");
-                                    match challenge.verify_user_answer() {
-                                        true => {
-                                            println!("     > Correct!");
-                                        }
-                                        false => {
-                                            println!("     x Uncorrect...");
-                                            println!(
-                                                "The correct answer was {}",
-                                                challenge.correct_answer
-                                            );
-                                        }
-                                    }
-                                }
-                                println!("Your current score is: {}\n", game.get_current_score());
-                                challenge_num += 1;
-                                if let Some(challenge) = game.challenges.get(challenge_num) {
-                                    println!("Challenge number {}. Listen...", challenge_num + 1);
-                                    challenge.play_correct_answer();
-                                    println!("What interval is it? (type 'replay' to replay the interval)");
-                                } else {
-                                    println!("Game is over!");
-                                    println!("Your final score is: {}", game.get_current_score());
-                                    session = None;
-                                    challenge_num = 0;
-                                }
-
-                            }
-                            Err(_) => println!("Invalid answer! (valid answers are : P1, m2, M2, m3 , M3, P4, P5, d5, m6, M6, m7, M7, P8")
-                        }
+            Ok(line) => match Command::from_str(&line) {
+                Ok(cmd) => match cmd {
+                    Command::Start => {
+                        session = Some(Game::new(2, Some(Note::C4), Some(vec![])));
                     }
-                }
-                None => match Command::from_str(&line) {
-                    Ok(cmd) => match cmd {
-                        Command::Start => {
-                            println!("Ok ! Let's go");
-                            session = Some(Game::new(0, Some(Note::C4), Some(vec![])));
-                            println!("Challenge number {}. Listen...", challenge_num + 1);
-                            session.as_ref().unwrap().challenges[challenge_num]
-                                .play_correct_answer();
-                            println!("What interval is it? (type 'replay' to replay the interval)");
-                        }
-                        Command::Quit => {
-                            println!("See You !");
-                            break;
-                        }
-                        _ => {
-                            println!("Wrong command! (type: 'start' to start and 'quit' to");
-                            println!("quit the game)");
-                        }
-                    },
-                    Err(_) => {
-                        println!("Wrong command! (type: 'start' to start and 'quit' to");
-                        println!("quit the game)");
+                    Command::Quit => {
+                        println!("See You!");
+                        break;
+                    }
+                    _ => {
+                        println!("Wrong command! (type: 'start' to start game and 'quit' to quit Jazz Gym)");
                     }
                 },
+                Err(_) => {
+                    println!("Wrong command! (type: 'start' to start a new game and 'quit' to quit Jazz Gym)");
+                }
             },
             Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
                 break;
             }
             Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
                 break;
             }
             Err(err) => {
